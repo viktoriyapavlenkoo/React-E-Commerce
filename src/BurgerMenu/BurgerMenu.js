78ae0ba21f06f01fcf7146b5377fef1059b3faad
Viktoriya <viktoriyapavlenkoo418@gmail.com>
2024-01-22 19:13:48 +0200

    Finished burger menu.

--- src/BurgerMenu/BurgerMenu.js
+++ src/BurgerMenu/BurgerMenu.js
@@ -2,11 +2,16 @@ import { Link } from 'react-router-dom';
 import './BurgerMenu.css';
 import Menu from '../Menu/Menu';
 import ButtonsList from '../ButtonsList/ButtonsList';
+import { useState } from 'react';
 
 function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
   return (
-   <div className='burger-menu'> 
-    <div className="burger-menu__icon">
+   <div className={`burger-menu ${isOpen ? 'open' : ''}`}> 
+    <div className="burger-menu__icon" onClick={toggleMenu}>
       <span></span>
     </div>
     <nav className="burger-menu__nav">
