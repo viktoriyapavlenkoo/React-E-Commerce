4780771588f0949b07a86b36b9d2cc85e39340ec
Viktoriya <viktoriyapavlenkoo418@gmail.com>
2024-01-26 15:40:50 +0200

    Created category component.

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
+import React from 'react';
 import './Category.css';
+import json from '../json/categoryData.json'
 
 function Category() {
+  const categoryData = json.data;
   return (
-   <section className=''> 
-    <h1>Category</h1>
+   <section className='section category'> 
+    <div className="container category__container">
+      <h2 className="section-title category__title">Browse The Range</h2>
+      <p className="text category__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <ul className="category__list">
+        {categoryData.map(item => 
+          <li key={item.id}>
+            <div className="category-item__img-block">
+              <img src={item.image} alt={item.name}/>
+            </div>
+           <h3 className="category-item__title item-title">{item.name}</h3>
+          </li>  
+        )}
+      </ul>
+    </div>
    </section>
   );
 }
--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
       <div className="hero__content">
         <h3 className="hero__subtitle">New Arrival</h3>
         <h1 className="title hero__title">Discover Our New Collection</h1>
-        <p className="text hero__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <p className="hero__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
         <button type='button' className='btn hero__btn'>Buy now</button>
       </div>
     </div>
