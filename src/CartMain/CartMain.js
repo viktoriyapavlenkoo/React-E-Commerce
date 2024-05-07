b4b5e0704f131a10b5ba5d240fb631e04f37c2e0
Viktoriia Ahafonova <viktoriyapavlenkoo418@gmail.com>
2024-05-07 21:47:02 +0300

    [VA][REC-39] Made cart table.

--- src/CartMain/CartMain.js
+++ src/CartMain/CartMain.js
@@ -8,6 +8,8 @@ function CartMain() {
   let cart = [];
   cart.push(productsData[0]);
   cart.push(productsData[1]);
+  cart.push(productsData[18]);
+  cart.push(productsData[14]);
   console.log(cart)
   return (
     <section className="section cart-main">
@@ -15,24 +17,26 @@ function CartMain() {
         <table className="cart__table cart-table table">
           <thead className="cart-table__thead">
             <tr className="cart-table__header">
-              <th className="cart-table__column-title column-title__item-product">Product</th>
+              <th className="cart-table__column-title column-title__item-product">
+                Product
+              </th>
               <th className="cart-table__column-title">Price</th>
               <th className="cart-table__column-title">Quantity</th>
               <th className="cart-table__column-title">Subtotal</th>
               <th className="cart-table__column-title">Delete</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="cart-table__tbody">
             {cart.map((product) => {
               console.log(product);
               return (
                 <tr key={product.id} className="cart-table__row">
-                  <CartProduct product={product} />
+                  <CartProduct product={product} cart={cart} />
                 </tr>
               );
             })}
           </tbody>
-          <tfoot>
+          <tfoot >
             <tr className="cart-table__tfoot">
               <th colSpan="2" className="cart-table__tfoot-item item-total">
                 Total
@@ -42,9 +46,9 @@ function CartMain() {
                 20000 {cart[0].currency}
               </td>
               <td className="cart-table__tfoot-item item-checkout">
-                <button type="button" className="item-checkout__btn ">
+                {/* <button type="button" className="item-checkout__btn">
                   Check out
-                </button>
+                </button> */}
               </td>
             </tr>
           </tfoot>
