fac9d011927751fee1a8142611ef828989aff122
Viktoriia Ahafonova <viktoriyapavlenkoo418@gmail.com>
2024-05-09 22:23:07 +0300

    [VA][REC-39] Started to add adaptive to cart page.

--- src/CartProduct/CartProduct.js
+++ src/CartProduct/CartProduct.js
@@ -8,41 +8,48 @@ function CartProduct(props) {
   const [count, setCount] = useState(1);
   return (
     <>
-      <div className="item-product__product-block item-product__item">
-        <div className="item-product__img-block">
-          <img src={product.images[0]} alt="" />
-        </div>
+      {/* <div className="item-product__product-block item-product__item"> */}
+      <div className="item-product__img-block">
+        <img src={product.images[0]} alt="" />
+      </div>
+
+      <div className="item-product__text-block">
         <p className="item-product__name">{product.name}</p>
+        {/* </div> */}
+        <div className="item-product__quantity-subtotal-block">
+          <p className="item-product__price item-product__item">
+            {product.price} {product.currency}
+          </p>
+
+          <div className="item-product__quantity item-product__item">
+            <button
+              type="button"
+              className="quantity__btn decrease-btn"
+              onClick={() => {
+                if (count > 1) {
+                  setCount(count - 1);
+                }
+              }}
+            >
+              -
+            </button>
+            <p className="quantity__text">{count}</p>
+            <button
+              type="button"
+              className="quantity__btn increase-btn"
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </button>
+          </div>
+
+          <p className="item-product__subtotal item-product__item">
+            {product.price * count} {product.currency}
+          </p>
+        </div>
       </div>
-      <p className="item-product__price item-product__item">
-        {product.price} {product.currency}
-      </p>
-      <div className="item-product__quantity item-product__item">
-        <button
-          type="button"
-          className="quantity__btn decrease-btn"
-          onClick={() => {
-            if (count > 1) {
-              setCount(count - 1);
-            }
-          }}
-        >
-          -
-        </button>
-        <p className="quantity__text">{count}</p>
-        <button
-          type="button"
-          className="quantity__btn increase-btn"
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <p className="item-product__subtotal item-product__item">
-        {product.price * count} {product.currency}
-      </p>
       <button
         type="button"
         className="item-product__delete-btn item-product__item"
