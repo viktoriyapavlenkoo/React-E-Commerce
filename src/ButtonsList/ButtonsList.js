08ac63c945241d2188c10e15acfb289839c8685b
Viktoriia Ahafonova <viktoriyapavlenkoo418@gmail.com>
2024-05-21 16:08:22 +0300

    [VA][REC-41] Added opening of the cart page by clicking on the cart icon.

--- src/ButtonsList/ButtonsList.js
+++ src/ButtonsList/ButtonsList.js
@@ -1,23 +1,27 @@
+import { Link } from 'react-router-dom';
 import './ButtonsList.css';
 
 function ButtonsList(className) {
   return (
     <ul className={`btns-list ${className.props}`}>
-      <li className='btns-list__item'>
-        <button className='btns-list__btn'>
-          <img src="../images/header/user-account-icon.svg" alt="User account icon" />
-        </button>
+      <li className="btns-list__item">
+        <Link to="#"  className="btns-list__btn">
+          <img
+            src="../images/header/user-account-icon.svg"
+            alt="User account icon"
+          />
+        </Link>
       </li>
-  
-      <li className='btns-list__item'>
-        <button className='btns-list__btn'>
+
+      <li className="btns-list__item">
+        <Link to="#" className="btns-list__btn">
           <img src="../images/header/favorites-icon.svg" alt="Favorites icon" />
-        </button>
+        </Link>
       </li>
-      <li className='btns-list__item'>
-        <button className='btns-list__btn'>
+      <li className="btns-list__item">
+        <Link to="/cart" className="btns-list__btn">
           <img src="../images/header/cart-icon.svg" alt="Cart icon" />
-        </button>
+        </Link>
       </li>
     </ul>
   );
