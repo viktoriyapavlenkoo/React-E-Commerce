e11db2e53bc006e8a761997162bf013680ab35a4
Viktoriia Ahafonova <viktoriyapavlenkoo418@gmail.com>
2024-06-10 13:02:37 +0300

    [VA][REC-47] Added column image to product table.

--- src/AdminProductTable /AdminProductTable .js
+++ src/AdminProductTable /AdminProductTable .js
@@ -4,6 +4,8 @@ function AdminProductTable() {
   const productData = [];
 
   productData.push({
+    image:
+      "/images/products/product-img-1.jpg",
     name: "Syltherinezz",
     articul: 1547,
     quantity: 20,
@@ -13,6 +15,7 @@ function AdminProductTable() {
     status: "Active",
   });
   productData.push({
+    image: "/images/products/product-img-2.jpg",
     name: "Lolito",
     articul: 1882,
     quantity: 16,
@@ -22,6 +25,7 @@ function AdminProductTable() {
     status: "Inactive",
   });
   productData.push({
+    image: "/images/products/product-img-3.jpg",
     name: "Respira",
     articul: 1547,
     quantity: 35,
@@ -31,6 +35,7 @@ function AdminProductTable() {
     status: "Promotion",
   });
   productData.push({
+    image: "/images/products/product-img-4.jpg",
     name: "Syltherine",
     articul: 2072,
     quantity: 10,
@@ -40,6 +45,7 @@ function AdminProductTable() {
     status: "Active",
   });
   productData.push({
+    image: "/images/products/product-img-1.jpg",
     name: "Syltherinezz",
     articul: 1547,
     quantity: 20,
@@ -49,6 +55,7 @@ function AdminProductTable() {
     status: "Active",
   });
   productData.push({
+    image: "/images/products/product-img-2.jpg",
     name: "Lolito",
     articul: 1882,
     quantity: 16,
@@ -58,6 +65,7 @@ function AdminProductTable() {
     status: "Inactive",
   });
   productData.push({
+    image: "/images/products/product-img-3.jpg",
     name: "Respira",
     articul: 1547,
     quantity: 35,
@@ -67,6 +75,7 @@ function AdminProductTable() {
     status: "Promotion",
   });
   productData.push({
+    image: "/images/products/product-img-4.jpg",
     name: "Syltherine",
     articul: 2072,
     quantity: 10,
@@ -75,84 +84,95 @@ function AdminProductTable() {
     currency: "UAH",
     status: "Active",
   });
-    productData.push({
-      name: "Syltherinezz",
-      articul: 1547,
-      quantity: 20,
-      category: "Dining",
-      price: "1500",
-      currency: "UAH",
-      status: "Active",
-    });
-    productData.push({
-      name: "Lolito",
-      articul: 1882,
-      quantity: 16,
-      category: "Living",
-      price: "2000",
-      currency: "UAH",
-      status: "Inactive",
-    });
-    productData.push({
-      name: "Respira",
-      articul: 1547,
-      quantity: 35,
-      category: "Bedroom",
-      price: "1200",
-      currency: "UAH",
-      status: "Promotion",
-    });
-    productData.push({
-      name: "Syltherine",
-      articul: 2072,
-      quantity: 10,
-      category: "Dining",
-      price: "1500",
-      currency: "UAH",
-      status: "Active",
-    });
-    productData.push({
-      name: "Syltherinezz",
-      articul: 1547,
-      quantity: 20,
-      category: "Dining",
-      price: "1500",
-      currency: "UAH",
-      status: "Active",
-    });
-    productData.push({
-      name: "Lolito",
-      articul: 1882,
-      quantity: 16,
-      category: "Living",
-      price: "2000",
-      currency: "UAH",
-      status: "Inactive",
-    });
-    productData.push({
-      name: "Respira",
-      articul: 1547,
-      quantity: 35,
-      category: "Bedroom",
-      price: "1200",
-      currency: "UAH",
-      status: "Promotion",
-    });
-    productData.push({
-      name: "Syltherine",
-      articul: 2072,
-      quantity: 10,
-      category: "Dining",
-      price: "1500",
-      currency: "UAH",
-      status: "Active",
-    });
+  productData.push({
+    image: "/images/products/product-img-1.jpg",
+    name: "Syltherinezz",
+    articul: 1547,
+    quantity: 20,
+    category: "Dining",
+    price: "1500",
+    currency: "UAH",
+    status: "Active",
+  });
+  productData.push({
+    image: "/images/products/product-img-2.jpg",
+    name: "Lolito",
+    articul: 1882,
+    quantity: 16,
+    category: "Living",
+    price: "2000",
+    currency: "UAH",
+    status: "Inactive",
+  });
+  productData.push({
+    image: "/images/products/product-img-3.jpg",
+    name: "Respira",
+    articul: 1547,
+    quantity: 35,
+    category: "Bedroom",
+    price: "1200",
+    currency: "UAH",
+    status: "Promotion",
+  });
+  productData.push({
+    image: "/images/products/product-img-4.jpg",
+    name: "Syltherine",
+    articul: 2072,
+    quantity: 10,
+    category: "Dining",
+    price: "1500",
+    currency: "UAH",
+    status: "Active",
+  });
+  productData.push({
+    image: "/images/products/product-img-1.jpg",
+    name: "Syltherinezz",
+    articul: 1547,
+    quantity: 20,
+    category: "Dining",
+    price: "1500",
+    currency: "UAH",
+    status: "Active",
+  });
+  productData.push({
+    image: "/images/products/product-img-2.jpg",
+    name: "Lolito",
+    articul: 1882,
+    quantity: 16,
+    category: "Living",
+    price: "2000",
+    currency: "UAH",
+    status: "Inactive",
+  });
+  productData.push({
+    image: "/images/products/product-img-3.jpg",
+    name: "Respira",
+    articul: 1547,
+    quantity: 35,
+    category: "Bedroom",
+    price: "1200",
+    currency: "UAH",
+    status: "Promotion",
+  });
+  productData.push({
+    image: "/images/products/product-img-4.jpg",
+    name: "Syltherine",
+    articul: 2072,
+    quantity: 10,
+    category: "Dining",
+    price: "1500",
+    currency: "UAH",
+    status: "Active",
+  });
   return (
     <section className="section admin-product-table admin-table-block">
       <div className="container admin-product-table__container admin-table-block__container">
         <table className="table admin-table admin-product-table__table">
           <thead className="admin-table__thead">
             <tr className="admin-table__thead-row admin-table__row">
+              <th className="admin-table__thead-cell admin-table__cell">
+                Image
+              </th>
               <th className="admin-table__thead-cell admin-table__cell">
                 Name
               </th>
@@ -177,6 +197,11 @@ function AdminProductTable() {
                   key={index}
                   className="admin-table__tbody-row admin-table__row"
                 >
+                  <td className="admin-table__tbody-cell admin-table__cell cell__image">
+                    <div className="cell-image__block">
+                      <img src={product.image} alt="" />
+                    </div>
+                  </td>
                   <td className="admin-table__tbody-cell admin-table__cell cell__name-articul">
                     <p className="name-text">{product.name}</p>
                     <p className="artitul-text">{product.articul}</p>
